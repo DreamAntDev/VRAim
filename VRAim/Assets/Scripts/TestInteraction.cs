fd1a83122f2b39ba314bd029aeb423cec38ce9fd
Nammu <mod157@naver.com>
2024-03-15 21:07:13 +0900

    Rig Direction Test

---
+++ VRAim/Assets/Scripts/TestInteraction.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+[SelectionBase]
+[DisallowMultipleComponent]
+[AddComponentMenu("XR/XR Test Interactable", 11)]
+public class TestInteraction : XRBaseInteractable
+{
+    
+}
